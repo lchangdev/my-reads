4eee1bee82f8811b3c2816843d70cae7ab690761
Lawrence Chang <lchangdev@gmail.com>
2017-07-27 15:12:53 -0700

    feat(search): Create Search components
    - Separate search page into 3 components
    - Ensure links between home and search page are linked correctly via
      react router

---
+++ src/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search-books">
+        <SearchBar />
+        <SearchResults />
+      </div>
+    );
+  }
+}
+
+export default Search;
